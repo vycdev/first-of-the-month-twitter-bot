5c661e97855b241c143b9b6407a16300bbcbb71b
Sandu Victor <vyctor661@gmail.com>
2022-08-18 14:28:27 +0300

    feat: routes and ohter stuff
    logger added
    error handling added
    body parser added
    
    put the port in the env file or by default it is 5000
    
    moved what was in Authenticate() in the /oauth route which will pick the url generated and automatically redirect the user to the twitter app autheticator or whatever its called

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,48 @@
 import "dotenv/config"
+import TwitterApi from "twitter-api-v2"
 
-console.log("hello world" + process.env.TEST);
+import koa from "koa"
+import Router from "koa-router"
+import logger from "koa-logger"
+import bodyParser from "koa-bodyparser"
+import { errorHandler } from "./error/errorHandler"
 
+const port = Number(process.env.PORT || 5000)
+const app = new koa();
+const router = new Router();
 
+const twitterClient = new TwitterApi({
+    clientId: process.env.CLIENT_ID || "",
+    clientSecret: process.env.CLIENT_SECRET || ""
+})
+
+router.get("/", async (ctx, next) => {
+    ctx.status = 200;
+    ctx.body = {
+        message: `Welcome to the app, to be redirected to the OAuth request link go to ${ctx.href}oauth`
+    }
+    await next();
+})
+
+const callbackURL = "http://127.0.0.1:5000/callback"
+
+router.get("/oauth", async (ctx, next) => {
+    const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(callbackURL, { scope: ['tweet.write', 'offline.access'] })
+
+    console.log(url, codeVerifier, state);
+
+    ctx.redirect(url);
+    await next();
+})
+
+app.use(errorHandler())
+app.use(bodyParser())
+app.use(logger())
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(port, () => {
+    console.info(`Koa app started and listening to port ${port}! 🚀`);
+})
+
+//state=svkgXCJ32la-Cp2jiXPi6.Dx7C.n5ha8&
+//code=V0ttN0JaU0FsYUJoM1pLek1aZlZ4SnQ4cy14SzJxN3lJX2dJaktZczJhdzA4OjE2NjA3NDQ1NzA3MzY6MToxOmFjOjE
